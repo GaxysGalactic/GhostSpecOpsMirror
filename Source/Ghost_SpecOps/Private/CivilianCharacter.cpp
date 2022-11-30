c57138c807a5a107c884acde6b80d5872a46b595
Santiago Rafael Campo Cotes <44332093+GaxysGalactic@users.noreply.github.com>
2022-11-30 11:09:42 -0500

    Civilian Class Basics
    + .h and .cpp files for Civilian
    + BP based on Civilian
    + Random variables for testing

---
+++ Source/Ghost_SpecOps/Private/CivilianCharacter.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CivilianCharacter.h"
+
+ACivilianCharacter::ACivilianCharacter() :
+	IsFrightened(false),
+	IsEssential(false)
+{
+}
---
+++ Source/Ghost_SpecOps/Public/CivilianCharacter.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCharacter.h"
+#include "CivilianCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GHOST_SPECOPS_API ACivilianCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	ACivilianCharacter();
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool IsFrightened;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool IsEssential;
+	
+};
